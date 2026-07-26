1dffcdcf087cfa593371a102b08e73aef82dc182
Morgan Ombati <morgan.ombati@student.moringaschool.com>
2026-07-26 14:57:48 +0300

    Configure Flask application and database setup

---
+++ server/app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_cors import CORS
+
+from app.config.settings import Config
+from app.config.db import db, migrate
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # Load configuration
+    app.config.from_object(Config)
+
+    # Enable CORS
+    CORS(app)
+
+    # Initialize extensions
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    @app.route("/")
+    def home():
+        return {
+            "message": "Smart Barber API is running!"
+        }
+
+    return app
